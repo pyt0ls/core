84ee781deed16a21456deeaaa1911d1a273746c3
pytols <130239705+pyt0ls@users.noreply.github.com>
2025-11-27 19:47:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "¿Estás seguro?",
+    "¿De verdad seguro?",
+    "¿Estás seguro?",
+    "Pookie, por favor...",
+    "¡Piénsalo!",
+    "Si dices que no, me pondré muy triste...",
+    "Me pondré muy triste...",
+    "Me pondré muy, muy triste...",
+    "Está bien, dejaré de preguntar...",
+    "¡Es broma, di que sí, por favor! ❤️" 
 ];
 
 let messageIndex = 0;
